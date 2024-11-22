fb021d9247a3466a73761a1991e35ee657e09749
vsri08 <srijawasteacc111@gmail.com>
2024-11-22 10:01:25 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,14 +1,6 @@
 print('hello world')
 import math
 T = int(input())
-
-while T > 0:
-    num = int(input())
-    sqrt = int(math.sqrt(num))
-
-    if sqrt * sqrt == num:
-        print('1')
-    else:
-        print('0')
-
-    T = T - 1
+S = int(input())
+Q=T+S
+print('sum',Q)
