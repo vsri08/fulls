26f832ecb54d548226b4f88fd9d6fe6a17849778
vsri08 <srijawasteacc111@gmail.com>
2024-11-22 09:52:52 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,5 +1,9 @@
 print('hello world')
-width, height = map(int, input().split())
-def rectanglePerimeter(width, height):
-   return ((width + height)*2)
-print(rectanglePerimeter(width, height))
+try:
+    width = input()
+    height = input()
+    def rectanglePerimeter(width, height):
+       return ((width + height)*2)
+    print(rectanglePerimeter(width, height))
+except EOFError as e:
+    print(end="")
